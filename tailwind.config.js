44339b53031a0d043ad1e4b93bd2beedf023dfa9
MigoiArmando <cleanngowebapp@gmail.com>
2025-11-15 09:36:24 +0800

    style: change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 },
             },
             fontFamily: {
-                inter: ['"Inter"', "serif"],
+                poppins: ['"Poppins"', "sans-serif"],
             },
         },
     },
